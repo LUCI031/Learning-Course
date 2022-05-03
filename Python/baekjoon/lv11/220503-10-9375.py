699ef87e3f19c3a320068c13d15dad929a419d2b
LUCI031 <wlsrb031@naver.com>
2022-05-03 22:34:12 +0900

    030522 TIL Python 2

--- Python/baekjoon/lv11/220503-10-9375.py
+++ Python/baekjoon/lv11/220503-10-9375.py
@@ -10,9 +10,12 @@ for _ in range(N):
     M = int(input())
     for _ in range(M):
         a,b = map(str,input().split())
-        b.rstrip()
+        b.strip('\n')
         clothe.append(a)
         type1.append(b)
-        for c,t in zip(clothe, type1):
-            dict[t] = dict.get(t,0) + 1 
-        print(dict)
+    for c,t in zip(clothe, type1):
+        dict[t] = dict.get(t,0) + 1 
+    answer = 1
+    for t in dict:
+        answer *= (dict[t] + 1)
+    print(answer-1)
